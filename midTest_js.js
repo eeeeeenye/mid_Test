f75cc37c3460a70d693b7212dac793e4b0673b11
eeeeeenye <mayosung113@hanmail.net>
2022-04-24 23:30:11 +0900

    mid

--- midTest_js.js
+++
@@ -1,7 +0,0 @@
-window.onload=function(){
-    var dialog = document.querySelector('.Info');
-    dialog.addEventListener('scroll',function() {
-        dialog.style.backgroundColor="black";
-        dialog.style.color='white';
-    })
-}
